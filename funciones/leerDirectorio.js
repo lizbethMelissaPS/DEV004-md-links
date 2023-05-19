d3628c3d85295ff8132a1922d7355a1099c556e1
lizbethMelissaPS <melissasud27@gmail.com>
2023-05-19 12:28:54 -0500

    fun leer direcrio

---
+++ funciones/leerDirectorio.js
@@ -0,0 +1,7 @@
+import { statSync} from "fs";
+/* recurcividad
+promise.all resolver todas la promesas */
+
+// verifica si es un directorio
+export const directory = (path) => statSync(path).isDirectory();
+console.log('DIRCTORY ', directory);
